b9261106882f2d6f54462ce96acec433a034e589
ryyhan <dayel.rehan@gmail.com>
2024-06-16 15:12:14 +0530

    tokenization functions added

--- tokenization.py
+++ tokenization.py
@@ -1,7 +1,12 @@
 import nltk
 nltk.download('punkt')
-from nltk.tokenize import sent_tokenize
 
-def Tokenization(corpus):
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+def sentenceTokenization(corpus):
     processed_text = sent_tokenize(corpus)
+    return processed_text
+
+def wordTokenization(corpus):
+    processed_text = word_tokenize(corpus)
     return processed_text
