6d602f4cef6799860aed24c740fdef5e3587e8e8
ryyhan <dayel.rehan@gmail.com>
2024-06-16 16:02:29 +0530

    stemming functions added

---
+++ stemming.py
@@ -0,0 +1,28 @@
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+
+def porterStemming(corpus):
+    processed_text=""
+    words = word_tokenize(corpus)
+    ps = PorterStemmer()
+    for x in words:
+        processed_text= processed_text + " " + (ps.stem(x))
+    return processed_text
+
+
+def regexpStemmer(corpus):
+    processed_text=""
+    words = word_tokenize(corpus)
+    rs = RegexpStemmer('ing$|able$|s$')
+    for x in words:
+        processed_text= processed_text + " " + (rs.stem(x))
+    return processed_text
+
+
+def snowballStemmer(corpus):
+    processed_text=""
+    words = word_tokenize(corpus)
+    ss = SnowballStemmer("english")
+    for x in words:
+        processed_text= processed_text + " " + (ss.stem(x))
+    return processed_text
